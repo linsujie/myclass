b3c8b00dc41911d5777006c29dd190d3bfaf632c
linsj <lin.sujie1234@gmail.com>
2015-02-11 16:58:35 +0800

    a collection of the usual output format that I use

---
+++ oformat/oformat.h
@@ -0,0 +1,19 @@
+/*********************************************************************
+The format to out put numbers
+ *********************************************************************/
+
+#ifndef FORMATE1
+#define FORMATE1 "%.5e\n"
+#endif
+
+#ifndef FORMATE2
+#define FORMATE2 "%.5e %.5e\n"
+#endif
+
+#ifndef FORMATE3
+#define FORMATE3 "%.5e %.5e %.5e\n"
+#endif
+
+#ifndef FORMATE4
+#define FORMATE4 "%.5e %.5e %.5e %.5e\n"
+#endif
