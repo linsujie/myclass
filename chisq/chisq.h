59997d96b3603d4d506c81230e75ce863fb92560
linsj <lin.sujie1234@gmail.com>
2017-03-03 23:10:50 +0800

    formatting chisq.h

--- chisq/chisq.h
+++ chisq/chisq.h
@@ -13,8 +13,9 @@
 
 #include "pArray.h"
 #include "spectrum.h"
-class chisq {
-  protected:
+class chisq
+{
+protected:
   static const char annota[2];
   static const spectrum zerospec;
   std::vector<std::vector<double> > E, F, sigma, total_sigma;
@@ -26,20 +27,23 @@ class chisq {
 #endif
   int addexperiment(const std::string& line);
 
-  int dealoutput(const std::string& filename, const std::ostringstream& os, std::ios_base::openmode outmode) const;
+  int dealoutput(const std::string& filename, const std::ostringstream& os,
+                 std::ios_base::openmode outmode) const;
 
-  public:
+public:
   chisq(const std::string& filename, double Eindx = 0);
   chisq(const chisq& another);
 
   int printsizes() const;
 
   int printdat(const std::string& filename = "null") const;
-  int printdat(const std::vector<int>& setnums, const std::string& filename = "null") const;
+  int printdat(const std::vector<int>& setnums,
+               const std::string& filename = "null") const;
   int printdat(int setnum, const std::string& filename = "null") const;
 
 #ifndef NO_ROOT
-  TGraphErrors* GetTGraphErrors(int setnum, double index = 0) const; // A TGraphErrors would be newed by this function, please delete it yourself
+  TGraphErrors* GetTGraphErrors(int setnum, double index = 0) const;
+  // A TGraphErrors would be newed by this function, please delete it yourself
   chisq(TGraphErrors* gr, double Eindx = 0);
   chisq(const std::vector<TGraphErrors*>& grs, double Eindx = 0);
 #endif
@@ -47,21 +51,31 @@ class chisq {
   int extra_sigma(const spectrum& sigma_ = zerospec);
 
   double chi2(const pArray& E_, const pArray& F_, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
-  double chi2(const std::vector<int>& setnums, const pArray& E_, const pArray& F_, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
+  double chi2(const std::vector<int>& setnums, const pArray& E_, const pArray& F_,
+              bool pflag = false,
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
   double chi2(int setnum, const pArray& E_, const pArray& F_, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
 
   double chi2(const spectrum& phi, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
-  double chi2(const std::vector<int>& setnums, const spectrum& phi, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
+  double chi2(const std::vector<int>& setnums, const spectrum& phi,
+              bool pflag = false,
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
   double chi2(int setnum, const spectrum& phi, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
 
-  private:
-  double chi2(int setnum, const double* E_, const double* F_, int nsize, bool pflag = false,
-              const std::string& filename = "null", std::ios_base::openmode mode = std::ios_base::out) const;
+private:
+  double chi2(int setnum, const double* E_, const double* F_, int nsize,
+              bool pflag = false,
+              const std::string& filename = "null",
+              std::ios_base::openmode mode = std::ios_base::out) const;
 };
 #endif // for #ifndef _CHISQ_H
